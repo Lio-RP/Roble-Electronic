8b2e9989f66f1b3df0393850e0e41a5cde46bbc1
liban Abdullahi <libanr4243@gmail.com>
2022-04-15 22:02:30 +0300

    Creating Spring Boot Project. closes #1

---
+++ src/main/java/com/liban/project/robleelectronic/RobleElectronicApplication.java
@@ -0,0 +1,13 @@
+package com.liban.project.robleelectronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobleElectronicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobleElectronicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liban/project/robleelectronic/RobleElectronicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liban.project.robleelectronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobleElectronicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
